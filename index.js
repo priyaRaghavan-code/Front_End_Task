a7275d7f21d44d4ce0dbe8c1fa9282a7faa93509
priyaRaghavan-code <priyabhuvanaj99@gmail.com>
2021-10-21 19:57:22 +0530

    Completed the nav-bar and card's

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", () => {
     chart: {
       //alignTicks: false,
       height: 150,
-      type: "line",
+      type: "area",
       width: 150,
       // inverted: "true",
     },
@@ -88,7 +88,7 @@ document.addEventListener("DOMContentLoaded", () => {
       verticalAlign: "top",
       y: 100,
     },
-    colors: ["#808EAA", "#8933FF"],
+    colors: ["#FFD2C1", "#FFFFFF"],
     series: [
       {
         data: [1, 5, 2, 6, 3],
@@ -141,6 +141,258 @@ document.addEventListener("DOMContentLoaded", () => {
       y: 100,
     },
     colors: ["#808EAA", "#8933FF"],
+    series: [
+      {
+        data: [1, 5, 2, 6, 3],
+        yAxis: 1,
+      },
+      {
+        data: [2.9, 6.5, 7.4, 8, 3],
+        yAxis: 1,
+      },
+    ],
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  Highcharts.chart("container3", {
+    chart: {
+      //alignTicks: false,
+      height: 150,
+      type: "line",
+      width: 150,
+      // inverted: "true",
+    },
+    yAxis: {
+      categories: ["12Am", "8Am", "4Am", "11Pm"],
+    },
+    yAxis: [
+      {
+        title: {
+          text: "",
+        },
+        gridLineWidth: 0,
+      },
+      {
+        title: {
+          text: "",
+        },
+        opposite: false,
+      },
+    ],
+    title: {
+      text: "",
+    },
+    legend: {
+      layout: "vertical",
+      backgroundColor: "#FFFFFF",
+      floating: true,
+      align: "none",
+      x: 70,
+      verticalAlign: "top",
+      y: 100,
+    },
+    colors: ["#808EAA", "#8933FF"],
+    series: [
+      {
+        data: [1, 5, 2, 6, 3],
+        yAxis: 1,
+      },
+      {
+        data: [2.9, 6.5, 7.4, 8, 3],
+        yAxis: 1,
+      },
+    ],
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  Highcharts.chart("container4", {
+    chart: {
+      //alignTicks: false,
+      height: 150,
+      type: "line",
+      width: 150,
+      // inverted: "true",
+    },
+    yAxis: {
+      categories: ["12Am", "8Am", "4Am", "11Pm"],
+    },
+    yAxis: [
+      {
+        title: {
+          text: "",
+        },
+        gridLineWidth: 0,
+      },
+      {
+        title: {
+          text: "",
+        },
+        opposite: false,
+      },
+    ],
+    title: {
+      text: "",
+    },
+    legend: {
+      layout: "vertical",
+      backgroundColor: "#FFFFFF",
+      floating: true,
+      align: "none",
+      x: 70,
+      verticalAlign: "top",
+      y: 100,
+    },
+    colors: ["#808EAA", "#8933FF"],
+    series: [
+      {
+        data: [1, 5, 2, 6, 3],
+        yAxis: 1,
+      },
+      {
+        data: [2.9, 6.5, 7.4, 8, 3],
+        yAxis: 1,
+      },
+    ],
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  Highcharts.chart("container5", {
+    chart: {
+      //alignTicks: false,
+      height: 100,
+      type: "area",
+      width: 100,
+      // inverted: "true",
+    },
+    yAxis: {
+      categories: ["12Am", "8Am", "4Am", "11Pm"],
+    },
+    yAxis: [
+      {
+        title: {
+          text: "",
+        },
+        gridLineWidth: 0,
+      },
+      {
+        title: {
+          text: "",
+        },
+        opposite: false,
+      },
+    ],
+    title: {
+      text: "",
+    },
+    legend: {
+      layout: "vertical",
+      backgroundColor: "#FFFFFF",
+      floating: true,
+      align: "none",
+      x: 70,
+      verticalAlign: "top",
+      y: 100,
+    },
+    colors: ["#4AD9C2"],
+    series: [
+      {
+        data: [1, 5, 2, 6, 3],
+        yAxis: 1,
+      },
+    ],
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  Highcharts.chart("container6", {
+    chart: {
+      //alignTicks: false,
+      height: 100,
+      type: "area",
+      width: 100,
+      // inverted: "true",
+    },
+    yAxis: {
+      categories: ["12Am", "8Am", "4Am", "11Pm"],
+    },
+    yAxis: [
+      {
+        title: {
+          text: "",
+        },
+        gridLineWidth: 0,
+      },
+      {
+        title: {
+          text: "",
+        },
+        opposite: false,
+      },
+    ],
+    title: {
+      text: "",
+    },
+    legend: {
+      layout: "vertical",
+      backgroundColor: "#FFFFFF",
+      floating: true,
+      align: "none",
+      x: 70,
+      verticalAlign: "top",
+      y: 100,
+    },
+    colors: ["#FFD2C1"],
+    series: [
+      {
+        data: [1, 5, 2, 6, 3],
+        yAxis: 1,
+      },
+    ],
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  Highcharts.chart("container7", {
+    chart: {
+      //alignTicks: false,
+      height: 100,
+      type: "area",
+      width: 100,
+      // inverted: "true",
+    },
+    yAxis: {
+      categories: ["12Am", "8Am", "4Am", "11Pm"],
+    },
+    yAxis: [
+      {
+        title: {
+          text: "",
+        },
+        gridLineWidth: 0,
+      },
+      {
+        title: {
+          text: "",
+        },
+        opposite: false,
+      },
+    ],
+    title: {
+      text: "",
+    },
+    legend: {
+      layout: "vertical",
+      backgroundColor: "#FFFFFF",
+      floating: true,
+      align: "none",
+      x: 70,
+      verticalAlign: "top",
+      y: 100,
+    },
+    colors: ["#FFD2C1", " #8933FF"],
     series: [
       {
         data: [1, 5, 2, 6, 3],
